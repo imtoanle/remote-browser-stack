969d67ee36a11f90b6515e5f6b5f51495b62dd4a
Toan Le <toanledev@gmail.com>
2026-08-30 10:04:08 +0700

    fix: use Chromium user-namespace sandbox

--- browser/start-browser.sh
+++ browser/start-browser.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 exec dbus-run-session -- chromium \
   --user-data-dir="$HOME/profile" \
+  --disable-setuid-sandbox \
   --no-first-run \
   --no-default-browser-check \
   "${START_URL:-about:blank}"
